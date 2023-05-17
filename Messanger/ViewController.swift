1d0e6725fa6af6210522c2f1fb6142ab40cb794d
Vitali Schumarov <vitalischumarov@Vitalis-Air-2.fritz.box>
2023-05-17 20:00:44 +0200

    first commit

---
+++ Messanger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messanger
+//
+//  Created by Vitali Schumarov on 17.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
