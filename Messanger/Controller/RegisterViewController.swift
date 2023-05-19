ce585d12d783b81e46c1d4c4ddafaddfb4f43c1a
Vitali Schumarov <vitalischumarov@Vitalis-MacBook-Air-2.fritz.box>
2023-05-19 14:36:37 +0200

    1. added CocoaPods 2. implemented Firebase 3. added authentification function

--- Messanger/Controller/RegisterViewController.swift
+++ Messanger/Controller/RegisterViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     @IBAction func registerPressed(_ sender: UIButton) {
-        self.performSegue(withIdentifier: "goToChat", sender: self)
+        if let email = emailTextfield.text {
+            if let password = passwordTextField.text {
+                Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                  print("User created!!")
+                    print(authResult)
+                    self.emailTextfield.text = ""
+                    self.passwordTextField.text = ""
+                }
+            }
+        }
     }
 }
